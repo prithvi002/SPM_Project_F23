f3054e7700202da5ca1c6b0380b6da2d08643d4b
Mounisha Vustepalli <mounisha94@gmail.com>
2023-10-25 15:19:44 -0400

    Adding GA to the site

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,26 @@
-import React,{useEffect, useState} from 'react'
-import { Login } from './Login';
-import { Register } from './Register';
+import React, {useEffect, useState} from 'react'
+import {Login} from './Login';
+import {Register} from './Register';
+import ReactGA from "react-ga";
 import './App.css';
 
-function App()
-{
-  const [currentForm, setCurrentForm] = useState('login');
-
-  const toggleForm = (formName) => {
-    setCurrentForm(formName);
-  }
-
-  return(
-    <div className = "App">
-      <img className="homepage-logo" referrerPolicy="no-referrer" src={process.env.PUBLIC_URL + "/infinity-travel-logo.png"} height="80px" width="120px"/>
-    {
-      currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm}/>
+function App() {
+    const [currentForm, setCurrentForm] = useState('login');
+  const TRACKING_ID = "G-GMVY4B99CC";// tracking id for GA
+  ReactGA.initialize(TRACKING_ID);
+    const toggleForm = (formName) => {
+        setCurrentForm(formName);
     }
-    </div>
-  )
+
+    return (
+        <div className="App">
+            <img className="homepage-logo" referrerPolicy="no-referrer"
+                 src={process.env.PUBLIC_URL + "/infinity-travel-logo.png"} height="80px" width="120px"/>
+            {
+                currentForm === "login" ? <Login onFormSwitch={toggleForm}/> : <Register onFormSwitch={toggleForm}/>
+            }
+        </div>
+    )
 }
 
 export default App
