d879476b52bb896c7ffeeae2d36f7b6215b86cd1
pranith-meka <pranith@vt.edu>
2023-10-25 13:52:41 -0400

    Add ability to type into form fields

--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -5,15 +5,16 @@ export const Login = (props) => {
     const [pass, setPass  ] = useState('');
 
     const handleSubmit = (e) => {
+        console.log(email);
         e.preventDefault();
     }
     return (
         <div className="auth-form-container">
             <form className="login-form" onSubmit={handleSubmit}>
                 <label htmlFor="email">email</label>
-                <input value={email} type="email" placeholder="youremail@gmail.com" id="email" name="email" />
+                <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="youremail@gmail.com" id="email" name="email" />
                 <label htmlFor="password">password</label>
-                <input value={pass} type="password" placeholder="**********" id="password" name="password" />
+                <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="**********" id="password" name="password" />
                 <button type="submit">Login</button>
             </form>
             <button className="link-btn" onClick={() => props.onFormSwitch('register')}>Don't have an account? Register here.</button>
--- client/src/Register.jsx
+++ client/src/Register.jsx
@@ -2,10 +2,12 @@ import React, {useState} from "react"
 import './App.css';
 export const Register = (props) => {
     const [email, setEmail] = useState('');
-    const [pass, setPass  ] = useState('');
-    const [name, setName  ] = useState('');
+    const [pass, setPass] = useState('');
+    const [name, setName] = useState('');
 
     const handleSubmit = (e) => {
+        console.log(name);
+        console.log(email);
         e.preventDefault();
     }
 
@@ -13,11 +15,11 @@ export const Register = (props) => {
         <div className="auth-form-container">
             <form className="register-form" onSubmit={handleSubmit}>
                 <label htmlFor="name">Full Name</label>
-                <input value={name} type="name" placeholder="Full Name" id="name" name="name" />
+                <input value={name} onChange={(e) => setName(e.target.value)} type="name" placeholder="Full Name" id="name" name="name" />
                 <label htmlFor="email">email</label>
-                <input value={email} type="email" placeholder="youremail@gmail.com" id="email" name="email" />
+                <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="youremail@gmail.com" id="email" name="email" />
                 <label htmlFor="password">password</label>
-                <input value={pass} type="password" placeholder="**********" id="password" name="password" />
+                <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="**********" id="password" name="password" />
                 <button type="submit">Login</button>
             </form>
             <button className="link-btn" onClick={() => props.onFormSwitch('login')}>Already have an account? Login here.</button>
