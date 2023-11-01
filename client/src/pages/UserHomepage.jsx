d793dcdfcdea78c665b13f549f6bf664272ddc2b
pranith-meka <pranith@vt.edu>
2023-11-01 15:22:22 -0400

    Added logout button

--- client/src/pages/UserHomepage.jsx
+++ client/src/pages/UserHomepage.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Notifications from "../components/Notifications";
+import { useNavigate } from "react-router-dom";
 
 export const UserHomepage = () => {
+    const navigate = useNavigate();
+    const handleLogout = () => {
+        navigate("/");
+      };
   return (
     <div className="welcome-page">
       <h1>Welcome Traveler! This is your homepage</h1>
 
       <Notifications />
+      <Link to="/" onClick={handleLogout} className="logout-button">
+          Logout
+        </Link>
     </div>
   );
 };
